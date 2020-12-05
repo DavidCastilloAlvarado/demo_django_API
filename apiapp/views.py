373494bb6c2b439f509520f3ca31ec68d5de3efa
David Castillo Alvarado <dcastilloa@uni.pe>
2020-12-05 12:28:53 -0500

    documentation details

--- apiapp/views.py
+++ apiapp/views.py
@@ -20,14 +20,17 @@ class Customer_API(APIView):
     serializer_income = serializers.Id_post_serializer
     serializer_class = serializers.CustomerSerializer
 
-    @swagger_auto_schema(tags=['List of Customers'])
+    @swagger_auto_schema(
+        responses={200: serializer_class(many=True)},
+        tags=['List of Customers'],
+    )
     def get(self, request, format=None):
         """"Return the whole list of customer allocated in our database """
         queryset = Customer.objects.all()
         queryset = self.serializer_class(queryset, many=True)
         return Response(queryset.data)
 
-    @swagger_auto_schema(request_body=serializer_income, tags=['One Customer'])
+    @swagger_auto_schema(request_body=serializer_income, responses={200: serializer_class}, tags=['One Customer'])
     def post(self, request, format=None):
         """ Return the client information by ID"""
         serializer = self.serializer_income(data=request.data)
